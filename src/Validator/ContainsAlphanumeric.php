a864f58651aa8813c150de34b4cdcc667b193e10
Mykola Martynov <mykola@herdwatch.com>
2023-09-15 16:30:34 +0300

    add named properties into attribute constructor

--- src/Validator/ContainsAlphanumeric.php
+++ src/Validator/ContainsAlphanumeric.php
@@ -7,6 +7,14 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 class ContainsAlphanumeric extends Constraint
 {
-    public $message = 'The string "{{ string }}" contains an illegal character: it can only contain letters or numbers.';
-    public $mode = 'strict'; // If the constraint has configuration options, define them as public properties
+    public string $message = 'The string "{{ string }}" contains an illegal character: it can only contain letters or numbers.';
+    public string $mode = 'strict'; // If the constraint has configuration options, define them as public properties
+
+    public function __construct(string $mode = null, string $message = null, array $groups = null, $payload = null)
+    {
+        parent::__construct([], $groups, $payload);
+
+        $this->mode = $mode ?? $this->mode;
+        $this->message = $message ?? $this->message;
+    }
 }
